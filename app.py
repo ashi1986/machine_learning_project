c3a5ad2f4ea76d14c61210383c851ff59e7fb225
ashi1986 <vermaashishmca@gmail.com>
2022-06-12 13:16:19 +0530

    updated app file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "starting machine lineraing project"
+    return "starting machine learning project"
 
 if __name__=="__main__":
     app.run(debug=True)
